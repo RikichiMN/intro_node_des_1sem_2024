a4ed167f784ad9765dc3435775e2f206157c2bb1
RikichiMN <henrique.nepomuceno@etec.sp.gov.br>
2024-02-15 20:41:10 -0300

    aula 15/02/24

---
+++ index.js
@@ -0,0 +1,56 @@
+console.log("Hello World!");
+
+let nome = 'Henrique'; 
+
+console.log(nome);
+
+let idade = 25;
+let altura = 1.75;
+
+console.log('Nome: ' + nome + ' - Idade: ' + idade + ' anos');
+console.log(`Nome: ${nome} - Idade: ${idade} anos`);
+
+let trabalha = true;
+
+console.log(trabalha);
+console.log('Trabalha: ' + (trabalha === true ? 'Sim' : 'Não'));
+
+if (trabalha === true) {
+    console.log('Trabalha: Sim');
+} else {
+    console.log('Trabalha: Não');
+}
+
+const sangue = 'B+';
+
+
+console.log(
+'vNome: ' + typeof(nome) + '\n' +
+'vIdade: ' + typeof(idade) + '\n' + 
+'vAltura: ' + typeof(altura) + '\n' +
+'vTrabalha: ' + typeof(trabalha) + '\n' +
+'vConstante: ' + typeof(sangue) + '\n'
+);
+
+let endereco = {
+    "rua" : "Brasil",
+    "num": 108,
+    "bairro" : "centro",
+    "cidade" : "tupã",
+    "uf" : "SP"
+}
+
+console.log(endereco);
+
+console.log(typeof(endereco));
+
+let frutas = ['banana', 'mamão', 'maçã'];
+console.log(frutas[0]);
+
+let velha = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]
+];
+
+console.log(velha[1][1]);
