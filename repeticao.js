eb810290e1f6176a62cf8ab782b54a13475823f8
RikichiMN <henrique.nepomuceno@etec.sp.gov.br>
2024-02-27 20:39:49 -0300

    aula 27/02

--- repeticao.js
+++ repeticao.js
@@ -32,7 +32,44 @@ let num = parseInt(entrada('Digite um número para visualizar a tabuada!'));
     //cont++;
 //}
 
-for (let cont = 1; cont <= 10; cont++) {
-    let calc = num * cont;
-    console.log(`${num} X ${cont} = ${calc}`);
+//for (let cont = 1; cont <= 10; cont++) {
+//    let calc = num * cont;
+//    console.log(`${num} X ${cont} = ${calc}`);
+//}
+
+//const senha = '123456';
+//let cont = 0;
+//do {
+//    if (cont === 3){
+//        break;
+//    }
+//    cont++;
+//    var senhaDig = entrada(`Digite a senha para acessar o sistema: `);
+//} while (senhaDig !== senha);
+
+//if (cont >= 3){
+//    console.log('Acesso não autorizado');
+//} else {
+//    console.log('Acesso autorizado');
+//}
+
+let senha = '123456';
+let snCorreta = false;
+
+for (let index = 0; index < 3; index++) {
+    const snDg = entrada('Digite a senha: ');
+    if (senha === snDg){
+        snCorreta = true;
+        break
+    }
 }
+
+console.log(snCorreta ? 'Acesso autorizado' : 'Acesso negado');
+entrada()
+
+//let sn = '123456';
+//let snDg = entrada('Digite a senha para acessar o sistema: ');
+
+//while (sn ==! snDg) {
+//    snDg = entrada('Digite a senha para acessar o sistema: ');
+//}
